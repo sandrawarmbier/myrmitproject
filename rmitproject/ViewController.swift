a8ac162ff43cb6881c526d218d48185fe30a6fbb
sandrawarmbier <warmbier.sandra@gmail.com>
2022-02-28 03:15:22 +1100

    update view controller

---
+++ rmitproject/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  rmitproject
+//
+//  Created by Sandra Warmbier on 28/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // SLW start home page feature for rmitproject
+        // SLW start initial page..
+        
+    }
+
+
+}
+
