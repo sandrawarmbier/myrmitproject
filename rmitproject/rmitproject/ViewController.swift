1bdfa9d37ba20ee92379af2419a0f7e68d6a18ca
sandrawarmbier <warmbier.sandra@gmail.com>
2022-02-28 01:36:39 +1100

    initial iOS commit
    initial iOS rmitproject commit

---
+++ rmitproject/rmitproject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rmitproject
+//
+//  Created by Sandra Warmbier on 28/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
